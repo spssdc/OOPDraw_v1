9904ea73e80c953db09730032598a2a77acc5a79
SDC@StPaulsSchool.org.uk <SDC@StPaulsSchool.org.uk>
2021-01-11 18:30:19 +0000

    Drag 'n drop added

--- OOPDraw.Designer.cs
+++ OOPDraw.Designer.cs
@@ -41,6 +41,9 @@
             this.Canvas.TabIndex = 0;
             this.Canvas.TabStop = false;
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
             // 
             // OOPDraw
             // 
--- OOPDraw.cs
+++ OOPDraw.cs
@@ -19,16 +19,35 @@ namespace OOPDraw
         }
 
         Pen currentPen = new Pen(Color.Black);
+        bool dragging = false;
+        Point startOfDrag = Point.Empty;
+        Point endOfDrag = Point.Empty;
+        Point lastMousePosition = Point.Empty;
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
-            Point a = new Point(20, 30);
-            Point b = new Point(400, 500);
-            Point c = new Point(700, 300);
-            gr.DrawLine(currentPen, a, b);
-            gr.DrawLine(currentPen, a, c);
-            gr.DrawLine(currentPen, b, c);
+            gr.DrawLine(currentPen,startOfDrag,lastMousePosition);
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startOfDrag = lastMousePosition = e.Location;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                lastMousePosition = e.Location;
+                Refresh();
+            };
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
         }
     }
 }
