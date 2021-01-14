d9d0db400c73350bea694429e74de2691a84afa7
SDC@StPaulsSchool.org.uk <SDC@StPaulsSchool.org.uk>
2021-01-14 18:19:59 +0000

    Line class implemented

--- OOPDraw.Designer.cs
+++ OOPDraw.Designer.cs
@@ -29,15 +29,19 @@
         private void InitializeComponent()
         {
             this.Canvas = new System.Windows.Forms.PictureBox();
+            this.LineWidth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Colour = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // Canvas
             // 
             this.Canvas.BackColor = System.Drawing.SystemColors.Window;
-            this.Canvas.Location = new System.Drawing.Point(224, 27);
+            this.Canvas.Location = new System.Drawing.Point(412, 61);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(1321, 807);
+            this.Canvas.Size = new System.Drawing.Size(1125, 734);
             this.Canvas.TabIndex = 0;
             this.Canvas.TabStop = false;
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
@@ -45,22 +49,75 @@
             this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
             this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
             // 
+            // LineWidth
+            // 
+            this.LineWidth.FormattingEnabled = true;
+            this.LineWidth.Items.AddRange(new object[] {
+            "Thin",
+            "Medium",
+            "Thick"});
+            this.LineWidth.Location = new System.Drawing.Point(12, 112);
+            this.LineWidth.Name = "LineWidth";
+            this.LineWidth.Size = new System.Drawing.Size(374, 40);
+            this.LineWidth.TabIndex = 1;
+            this.LineWidth.SelectedIndexChanged += new System.EventHandler(this.LineWidth_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Line Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Line Colour";
+            // 
+            // Colour
+            // 
+            this.Colour.FormattingEnabled = true;
+            this.Colour.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue"});
+            this.Colour.Location = new System.Drawing.Point(12, 252);
+            this.Colour.Name = "Colour";
+            this.Colour.Size = new System.Drawing.Size(242, 40);
+            this.Colour.TabIndex = 4;
+            this.Colour.SelectedIndexChanged += new System.EventHandler(this.Colour_SelectedIndexChanged);
+            // 
             // OOPDraw
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1594, 853);
+            this.Controls.Add(this.Colour);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LineWidth);
             this.Controls.Add(this.Canvas);
             this.Name = "OOPDraw";
             this.Text = "OOPDraw";
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ComboBox LineWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Colour;
     }
 }
 
--- OOPDraw.cs
+++ OOPDraw.cs
@@ -15,39 +15,81 @@ namespace OOPDraw
         public OOPDraw()
         {
             InitializeComponent();
-            DoubleBuffered = true;
+            DoubleBuffered = true;  // Stops image flickering
         }
 
-        Pen currentPen = new Pen(Color.Black);
-        bool dragging = false;
-        Point startOfDrag = Point.Empty;
-        Point endOfDrag = Point.Empty;
-        Point lastMousePosition = Point.Empty;
+        Pen currentPen = new Pen(Color.Black);  // set up pen parameters
+        bool dragging = false;                  // not initially in drag mode
+        Point startOfDrag = Point.Empty;        // initialise line start point
+        Point lastMousePosition = Point.Empty;  // initialise most position
+        List<Line> lines = new List<Line>();    // create list of type Line
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
-            Graphics gr = e.Graphics;
-            gr.DrawLine(currentPen,startOfDrag,lastMousePosition);
+           Graphics gr = e.Graphics;
+           foreach (Line line in lines)
+            {
+                line.Draw(gr);
+            }
         }
 
         private void Canvas_MouseDown(object sender, MouseEventArgs e)
         {
             dragging = true;
             startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));  // initialise line (point)
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
             {
+                Line currentLine = lines.Last();  // list function for last item
+                currentLine.GrowTo(e.X, e.Y);     // invoke method to update the line
                 lastMousePosition = e.Location;
-                Refresh();
+                Refresh();                        // clear canvas & then canvas_paint called
             };
         }
 
         private void Canvas_MouseUp(object sender, MouseEventArgs e)
         {
             dragging = false;
+        }
+
+        private void LineWidth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float width = currentPen.Width;
+            switch (LineWidth.Text)
+            {
+                case "Thin":
+                    width = 2.0F;
+                    break;
+                case "Medium":
+                    width = 4.0F;
+                    break;
+                case "Thick":
+                    width = 8.0F;
+                    break;
+            }
+            currentPen = new Pen(currentPen.Color, width);
+        }
+
+        private void Colour_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color color = currentPen.Color;
+            switch (Colour.Text)  // read current text property of combo box
+            {
+                case "Red":
+                    color = Color.Red;
+                    break;
+                case "Green":
+                    color = Color.Green;
+                    break;
+                case "Blue":
+                    color = Color.Blue;
+                    break;
+            }
+            currentPen = new Pen(color, currentPen.Width);
         }
     }
 }
